ce52168c6eb642006b6e19cefcf0b00242ecdac8
eldovyn <farras.pramudita@gmail.com>
2025-10-28 20:36:27 +0700

    feat: add model user

---
+++ src/models/users.js
@@ -0,0 +1,42 @@
+// models/user.cjs
+const mongoose = require('mongoose');
+const { Schema, model, models } = mongoose;
+
+const RoleTypes = Object.freeze({
+    ADMIN: 'admin',
+    USER: 'user',
+});
+
+const UserSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 7,
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        role: {
+            type: String,
+            enum: Object.values(RoleTypes),
+            required: true,
+            default: RoleTypes.USER,
+        },
+    },
+    { timestamps: true }
+);
+
+UserSchema.index({ email: 1 }, { unique: true });
+
+const User = models.User || model('User', UserSchema);
+module.exports = { default: User, RoleTypes };
