8730b25d28274c685ade33227f4e87ff408eb433
eldovyn <farras.pramudita@gmail.com>
2025-11-03 22:17:22 +0700

    style(navbar): update style navbar

--- src/app/page.js
+++ src/app/page.js
@@ -12,15 +12,10 @@ export default function Home() {
             <ul
               tabindex="-1"
               class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul class="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+              <li><a>Produk Monitor</a></li>
+              <li><a>Produk Laptop</a></li>
+              <li><a>Produk Meja</a></li>
+              <li><a>Produk Kursi</a></li>
             </ul>
           </div>
           <a class="btn btn-ghost text-xl">daisyUI</a>
